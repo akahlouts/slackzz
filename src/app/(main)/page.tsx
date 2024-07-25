5db399fa6bc9cbbc9f617c8fbf7f1196d988ad0e
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-25 20:14:23 +0300

    implement getCurrentUser session function.

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+
+import { getUserData } from "@/actions/get-user-data";
+
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const userData = await getUserData();
+
+  if (!userData) return redirect("/auth");
+
+  const userWorkspaceId = userData.workspaces?.[0];
+
+  if (!userWorkspaceId) return redirect("/create-workspace");
+
+  if (userWorkspaceId) return redirect(`/workspace/${userWorkspaceId}`);
+
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
