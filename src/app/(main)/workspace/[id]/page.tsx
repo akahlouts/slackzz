8807db33fb82539d87057812966fff670b3b9ca9
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-27 20:39:31 +0300

    Fetch Workspaces & Workspaces Data.

--- src/app/(main)/workspace/[id]/page.tsx
+++ src/app/(main)/workspace/[id]/page.tsx
@@ -1,4 +1,23 @@
-const Workspace = () => {
+import { redirect } from "next/navigation";
+
+import { getUserData } from "@/actions/get-user-data";
+import {
+  getCurrentWorkspaceData,
+  getUserWorkspaceData,
+} from "@/actions/workspaces";
+
+const Workspace = async ({ params: { id } }: { params: { id: string } }) => {
+  const userData = await getUserData();
+
+  if (!userData) return redirect("/auth");
+
+  const [userWorkspaceData, userWorkspaceError] = await getUserWorkspaceData(
+    userData.workspaces!
+  );
+
+  const [currentWorkspaceData, currentWorkspaceError] =
+    await getCurrentWorkspaceData(id);
+
   return <div>Workspace</div>;
 };
 
