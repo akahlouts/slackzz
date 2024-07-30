7a78d3a30f6222daee812d73d43d23217e295e24
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-30 03:56:40 +0300

    Implement Text Editor, Menu Bar & Emoji.

--- src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
+++ src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
@@ -2,7 +2,9 @@ import { redirect } from "next/navigation";
 
 import Sidebar from "@/components/sidebar";
 import InfoSection from "@/components/info-section";
+import ChatHeader from "@/components/chat-header";
 import Typography from "@/components/ui/typography";
+import TextEditor from "@/components/text-editor";
 
 import { getUserData } from "@/actions/get-user-data";
 import {
@@ -31,21 +33,37 @@ const ChannelId = async ({
     userData.id
   );
 
+  const currentChannelData = userWorkspaceChannels.find(
+    (channel) => channel.id === channelId
+  );
+
+  if (!currentChannelData) return redirect("/");
+
   return (
     <div className="hidden md:block">
-      <Sidebar
-        currentWorkspaceData={currentWorkspaceData}
-        userData={userData}
-        userWorkspacesData={userWorkspaceData as UserWorkspace[]}
-      />
-      <InfoSection
-        currentWorkspaceData={currentWorkspaceData}
-        userData={userData}
-        userWorkspaceChannels={userWorkspaceChannels}
-        currentChannelId={channelId}
-      />
-      <div className="p-2">
-        <Typography text="Channel ID" variant="p" />
+      <div className="h-[calc(100vh-256px)] overflow-y-auto [&::-webkit-scrollbar-thumb]:rounded-[6px] [&::-webkit-scrollbar-thumb]:bg-foreground/60 [&::-webkit-scrollbar-track]:bg-none [&::-webkit-scrollbar]:w-2">
+        <Sidebar
+          currentWorkspaceData={currentWorkspaceData}
+          userData={userData}
+          userWorkspacesData={userWorkspaceData as UserWorkspace[]}
+        />
+        <InfoSection
+          currentWorkspaceData={currentWorkspaceData}
+          userData={userData}
+          userWorkspaceChannels={userWorkspaceChannels}
+          currentChannelId={channelId}
+        />
+        <div className="p-4 relative w-full overflow-hidden">
+          <ChatHeader title={currentChannelData.name} />
+
+          <div className="mt-10">
+            <Typography text="Chat Content" variant="h4" />
+          </div>
+        </div>
+      </div>
+
+      <div className="m-4">
+        <TextEditor />
       </div>
     </div>
   );
---
+++ src/components/chat-header.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+
+import { IoMdHeadset } from "react-icons/io";
+
+import Typography from "./ui/typography";
+
+type ChatHeaderProps = { title: string };
+
+const ChatHeader: FC<ChatHeaderProps> = ({ title }) => {
+  return (
+    <div className="absolute h-10 top-0 left-0 w-full">
+      <div className="h-10 flex items-center justify-between px-4 fixed md:w-[calc(100%-296px)] lg:w-[calc(100%-436px)] bg-white dark:bg-neutral-800 border-b border-b-white/30 shadow-md">
+        <Typography text={`# ${title}`} variant="h4" />
+        <IoMdHeadset className="text-primary" size={24} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
